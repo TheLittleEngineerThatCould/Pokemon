aa55d7158b73266c2e6da0e855df1b65bb32adb0
TheLittleEngineerThatCould <106122777+TheLittleEngineerThatCould@users.noreply.github.com>
2022-08-02 16:49:54 -0400

    connect to my database, can see json in browser

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require('express')
 require('dotenv').config() //telling express we want to use .env file to use
 const app = express()
 const port = process.env.PORT || 3003
+const pokemon = require('./models/pokemon')
 
 app.get('/', (req,res) => {
     res.send('Welcome to the Pokemon App')
 })
 
+app.get('/pokemon', (req,res) => {
+    res.send(pokemon)
+})
+
+
 app.listen(port,() => {
     console.log('I am listening on', port)
 })
+
+
