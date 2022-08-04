edcf93e71c584a24e6de47b7bcc95cc36e4618c9
TheLittleEngineerThatCould <106122777+TheLittleEngineerThatCould@users.noreply.github.com>
2022-08-04 09:43:23 -0400

    fixed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('dotenv').config() 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const pokemon = require('./models/pokemon')
 const mongoose = require('mongoose');
 const Pokemon = require('./models/pokemon')
